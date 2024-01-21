570116dd0050fabd857b667abbbf91e36da02c1e
ahmed-niaz <niazahmed.an@gmail.com>
2024-01-21 20:30:01 +0600

    banner section almost complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
-    
     extend: {
       colors: {
         "brown-shade": "#B68C5A",
-        "font-color":"var(--Dark-03, #707070)",
+        "font-color": "var(--Dark-03, #707070)",
       },
-      fontFamily:{
-        "garamond":['EB Garamond'],
-        "poppins":['Poppins'],
-
-      }
+      fontFamily: {
+        garamond: ["EB Garamond"],
+        poppins: ["Poppins"],
+      },
     },
   },
   plugins: [require("daisyui")],
